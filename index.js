e0c6b5b361ad107737e7f8f81324049e18058567
evaparicio Evelyn <ev.aparicio.c@gmail.com>
2018-07-14 20:54:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() * 20;
 
-let randomInteger = Math.floor(randomNumber)+1;
+let randomInteger = Math.floor(randomNumber);
 
 let randomUserID = randomNumber + 1000000000;
